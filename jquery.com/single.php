d6f1ca2bd4a415d70fde0e8edc151ef149f11e1d
Scott González <scott.gonzalez@gmail.com>
2013-01-18 00:46:07 -0500

    Updated author styling.

--- jquery.com/single.php
+++ jquery.com/single.php
@@ -5,6 +5,11 @@
 <div class="content-right twelve columns">
 	<div id="content">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-posted">
+			<?php twentyeleven_posted_on(); ?>
+		</div>
+		<?php endif; ?>
 		<hr>
 
 		<?php get_template_part( 'content', 'page' ); ?>
