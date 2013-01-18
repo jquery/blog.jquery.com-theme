8e8cd9b0e74ec2238a70ff13089055f8bc219f15
Scott González <scott.gonzalez@gmail.com>
2013-01-18 11:33:49 -0500

    Define JQUERY_LIVE_SITE.

--- jquery.com/functions.php
+++ jquery.com/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( !defined( 'JQUERY_LIVE_SITE' ) ) {
+	define( 'JQUERY_LIVE_SITE', 'blog.jquery.com' );
+}
+
 // Add body classes found in postmeta.
 add_filter( 'body_class', function( $classes ) {
 	$classes = array( 'jquery' );
