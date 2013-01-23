07c9daf414bea4acf90920dbf34b9bec2c77230c
Scott González <scott.gonzalez@gmail.com>
2013-01-23 16:33:37 -0500

    Don't show drafts in the sidebar.

--- jquery.com/sidebar.php
+++ jquery.com/sidebar.php
@@ -13,7 +13,9 @@
 		<h3 class="widget-title">Recent Posts</h3>
 		<ul>
 			<?php
-			$recent_posts = wp_get_recent_posts();
+			$recent_posts = wp_get_recent_posts(array(
+				'post_status' => 'publish'
+			));
 			foreach( $recent_posts as $recent ){
 				echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"].'</a></li>';
 			}
