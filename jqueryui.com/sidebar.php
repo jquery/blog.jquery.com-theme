a4ccd8c7edea9308e04bce73db78bb9bdfdd6fdd
Scott González <scott.gonzalez@gmail.com>
2013-11-12 07:26:32 -0500

    Revert jqueryui.com/sidebar.php changes.

--- jqueryui.com/sidebar.php
+++ jqueryui.com/sidebar.php
@@ -1,26 +1,31 @@
 <div id="sidebar" class="widget-area" role="complementary">
 	<aside class="widget">
-		<h3 class="widget-title">Interactions</h3>
+		<h3 class="widget-title">Categories</h3>
 		<ul>
-			<?php echo jq_page_links_for_category( 'interactions' ); ?>
+			<?php wp_list_categories(array(
+				'depth' => 1,
+				'title_li' => '',
+				'exclude' => '1'
+			)); ?>
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title">Widgets</h3>
+		<h3 class="widget-title">Recent Posts</h3>
 		<ul>
-			<?php echo jq_page_links_for_category( 'widgets' ); ?>
+			<?php
+			$recent_posts = wp_get_recent_posts(array(
+				'post_status' => 'publish'
+			));
+			foreach( $recent_posts as $recent ){
+				echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"].'</a></li>';
+			}
+			?>
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title">Effects</h3>
+		<h3 class="widget-title">Archives</h3>
 		<ul>
-			<?php echo jq_page_links_for_category( 'effects' ); ?>
-		</ul>
-	</aside>
-	<aside class="widget">
-		<h3 class="widget-title">Utilities</h3>
-		<ul>
-			<?php echo jq_page_links_for_category( 'utilities' ); ?>
+			<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 		</ul>
 	</aside>
 </div>
